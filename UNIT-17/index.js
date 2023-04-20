3f66e366ad382045f2e808fb0b3f451ea6126757
Demetris <demetris@mail.ru>
2023-04-20 19:13:05 +0500

    добавил 17 урок

---
+++ UNIT-17/index.js
@@ -0,0 +1,28 @@
+"use strict";
+let number = 123;
+let string = 'это строчная переменная';
+let isVerifiedPlus = true;
+let isVerifiedMinus = false;
+let zero = null;
+let xz = undefined;
+let item = Symbol('Yes')
+let place = {x: 10, y: 15}
+const pi=3.1415926;
+let country = {     
+    name: 'Russia',  
+    capital: 'Moscow',
+    language: 'rus',
+    area: 17098046,
+    population: 147182123,
+    currency: 'RUB',
+    callingCode: '+7',
+
+  }
+
+  
+
+console.log(number+string);
+console.log(country);
+
+
+
